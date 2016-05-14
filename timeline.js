a0c1ed71e395a2c1457d5af747de4f1a89c2acd0
Bella Norvig <isabella_norvig@brown.edu>
2016-05-13 21:46:33 -0400

    make a thing?

--- timeline.js
+++ timeline.js
@@ -1,4 +1,16 @@
 var chart = d3.timeline();
 
+var data = [
+	{label: "Life events", times: [
+		{label: "Born", starting_time: -2243358000000, display: "circle"}
+	]}// ,
+// 	{label: "Novels", times: [
+//
+// 	]},
+// 	{label: "Essays", times: [
+//
+// 	]}
+]
+
 var svg = d3.select("#timeline").append("svg").attr("width", 500)
-  .datum(testData).call(chart);
+  .datum(data).stack().call(chart);
