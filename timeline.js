49b6968ee2d21c7cb904970a6a90655ac54b9c87
Bella Norvig <isabella_norvig@brown.edu>
2016-05-13 22:26:49 -0400

    functionize

--- timeline.js
+++ timeline.js
@@ -1,21 +1,28 @@
-// window.onload = function() {
-// 	var chart = d3.timeline().stack();
+window.onload = function() {
+	
+
+	var data = [
+	{label: "Life events", times: [
+		{label: "Born", starting_time: -2243358000000, display: "circle"}
+	]}// ,
+// 	{label: "Novels", times: [
 //
-// 	var data = [
-// 	{label: "Life events", times: [
-// 		{label: "Born", starting_time: -2243358000000, display: "circle"}
-// 	]}// ,
-// // 	{label: "Novels", times: [
-// //
-// // 	]},
-// // 	{label: "Essays", times: [
-// //
-// // 	]}
-// 	];
+// 	]},
+// 	{label: "Essays", times: [
 //
-// 	var svg = d3.select("#timeline").append("svg").attr("width", 500)
-//   	.datum(data).call(chart);
-// }
+// 	]}
+	];
+
+	function timelineDraw() {
+		var chart = d3.timeline().stack();
+		
+		var svg = d3.select("#timeline").append("svg").attr("width", 500)
+	  	.datum(data).call(chart);
+	}
+	
+	timelineDraw();
+	
+}
 
     window.onload = function() {
       var testData = [
